b5005e39e8a5ffe9b732f271949f4b14e1f3eda9
Michael Glitzos <michael.glitzos@gmail.com>
2016-12-28 16:41:24 +0000

    Moved GraphQL from CLI to HTTP-based interface

--- graphql-project/server.js
+++ graphql-project/server.js
@@ -2,9 +2,14 @@ import {
     graphql
 }
 from 'graphql';
-import readline from 'readline';
+// import readline from 'readline';
 
 import mySchema from './schema/main';
+
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+
+const app = express();
 
 import {
     MongoClient
@@ -18,15 +23,28 @@ MongoClient.connect(MONGO_URL, (err, db) => {
     assert.equal(null, err);
     console.log('Connected to MongoDB server');
 
-    const rli = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
+    app.use('/graphql', graphqlHTTP({
+        schema: mySchema,
+        context: {
+            db
+        }
+    }));
 
-    rli.question('Client Request:', inputQuery => {
-        graphql(mySchema, inputQuery, {}, { db }).then(result => {
-            console.log('Server Answer :', result.data);
-            db.close(() => rli.close());
-        });
-    });
+    const port = process.env.PORT || 3000;
+
+    app.listen(port, () => {
+        console.info(`Running server on ${port}...`);
+    })
+
+    // const rli = readline.createInterface({
+    //     input: process.stdin,
+    //     output: process.stdout
+    // });
+
+    // rli.question('Client Request:', inputQuery => {
+    //     graphql(mySchema, inputQuery, {}, { db }).then(result => {
+    //         console.log('Server Answer :', result.data);
+    //         db.close(() => rli.close());
+    //     });
+    // });
 });
