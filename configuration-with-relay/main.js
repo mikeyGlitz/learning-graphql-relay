d16626a7ad6fe6874d3e5df6c5504d1313d978bd
Michael Glitzos <michael.glitzos@gmail.com>
2017-01-05 20:19:43 +0000

    setting up webpack

--- configuration-with-relay/main.js
+++ configuration-with-relay/main.js
@@ -19,6 +19,8 @@ MongoClient.connect(MONGO_URL, (err, db) => {
         graphiql: true
     }));
     
+    app.use(express.static(`${__dirname}/public`));
+    
     const port = process.env.PORT || 3000;
     
     app.listen(port, () => console.log(`Server running on ${port}...`));
